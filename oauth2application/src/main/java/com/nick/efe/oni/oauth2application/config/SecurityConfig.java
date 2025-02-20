d9c7d1a3d14ace9560c5cc8faabf196f26069aa3
Nick Efe Oni <neoni1@sheffield.ac.uk>
2025-02-20 20:48:19 +0000

    Filters understanding mastery check

--- oauth2application/src/main/java/com/nick/efe/oni/oauth2application/config/SecurityConfig.java
+++ oauth2application/src/main/java/com/nick/efe/oni/oauth2application/config/SecurityConfig.java
@@ -1,14 +1,17 @@
 package com.nick.efe.oni.oauth2application.config;
 
 import com.nick.efe.oni.oauth2application.config.filter.LoggingFilter;
+import jakarta.websocket.Session;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.context.SecurityContextHolderFilter;
 import org.springframework.security.web.context.request.async.WebAsyncManagerIntegrationFilter;
 
 @Configuration
@@ -17,20 +20,35 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable);
+        http//TODO cors filter
+                .csrf(AbstractHttpConfigurer::disable);
 
         http.addFilterBefore(new LoggingFilter(), WebAsyncManagerIntegrationFilter.class)
                 ;
 
-        //TODO add authentication
-        //TODO jwt
-        //TODO OAuth2
-        //TODO Basic Authentication
+        //TODO logout filter
+        //TODO usernamePasswordAuthenticationFilter
+        //TODO defaultLogoutPageGeneratingFilter
+        //TODO defaultLoginPageGeneratingFilter
+        //TODO Basic Authentication (Authorization: Basic ...)
+        //TODO BearerTokenAuthenticationFilter (jwt)
+        //TODO RequestCacheAwareFilter
+        //TODO
 
+        //SessionManagementFilter
+        http.sessionManagement(session
+                -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        );
+
+        // Security filter Chain interceptor
         http.authorizeHttpRequests(authorizeRequests -> {
-            authorizeRequests
-                    .requestMatchers(HttpMethod.GET, "/", "/favicon.ico", "/error").permitAll()
-                    .anyRequest().authenticated();
+            authorizeRequests.requestMatchers(HttpMethod.GET, "/", "/favicon.ico", "/error").permitAll();
+            authorizeRequests.requestMatchers("/basic").hasRole("BASIC");
+            authorizeRequests.requestMatchers("/intermediate").hasRole("INTERMEDIATE");
+            authorizeRequests.requestMatchers("/advanced").hasRole("ADVANCED");
+            authorizeRequests.requestMatchers("/master").hasRole("MASTER");
+            authorizeRequests.requestMatchers("/supreme").hasRole("SUPREME");
+            authorizeRequests.anyRequest().authenticated();
         });
 
 
